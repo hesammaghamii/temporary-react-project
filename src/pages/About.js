16dd17589738788460209e824f41f8d51f8b50b6
Hesam Maghami Rad <hesammaghami@gmail.com>
2023-11-01 00:26:25 +0330

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
+        voluptate esse exercitationem consectetur officiis eveniet, facilis
+        optio voluptatem earum modi! Facilis praesentium, est ut architecto quis
+        eos sequi ipsum debitis expedita nisi rem perferendis accusamus
+        temporibus eligendi corporis neque deserunt?
+      </p>
+    </section>
+  );
+};
+
+export default About;
